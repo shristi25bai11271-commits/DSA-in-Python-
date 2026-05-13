49fc35519fd06265b12de813c5a187a997421693
Shristi Rawat <shristi.25bai11271@vitbhopal.ac.in>
2026-05-13 10:56:11 +0530

    Update Bubble_sort.py

--- Sorting_Algorithms /Bubble_sort.py
+++ Sorting_Algorithms /Bubble_sort.py
@@ -1,1 +1,14 @@
+#creating a bubble sort algorithm 
+mylist = [7, 3, 9, 12, 11]
 
+n = len(mylist)
+for i in range(n-1):
+  swapped = False
+  for j in range(n-i-1):
+    if mylist[j] > mylist[j+1]:
+      mylist[j], mylist[j+1] = mylist[j+1], mylist[j]
+      swapped = True
+  if not swapped:
+    break
+
+print(mylist)
